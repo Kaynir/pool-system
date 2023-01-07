1998a68f636e9da6da28d58114baa87a607b0f34
Kaynir <kaynir.games@gmail.com>
2023-01-07 19:18:53 +0300

    Улучшил работу с частицей из пула.

--- Runtime/Scripts/PoolableParticle.cs
+++ Runtime/Scripts/PoolableParticle.cs
@@ -1,20 +1,34 @@
 using UnityEngine;
+using static UnityEngine.ParticleSystem;
 
 namespace Kaynir.Pools
 {
     [RequireComponent(typeof(ParticleSystem))]
     public class PoolableParticle : PoolableObject
     {
+        private ParticleSystem _system;
+        private Transform _transform;
+        private MainModule _main;
+        
         private void Awake()
         {
-            ParticleSystem system = GetComponent<ParticleSystem>();
-            ParticleSystem.MainModule main = system.main;
-            main.stopAction = ParticleSystemStopAction.Callback;
+            _system = GetComponent<ParticleSystem>();
+            _transform = transform;
+            _main = _system.main;
+            _main.stopAction = ParticleSystemStopAction.Callback;
         }
 
-        private void OnParticleSystemStopped()
+        public void Play(Vector3 position, MinMaxGradient gradient)
         {
-            Release();
+            _main.startColor = gradient;
+            _transform.position = position;
+            _system.Play(true);
         }
+
+        public void Play(Vector3 position) => Play(position, _main.startColor);
+
+        public void Play() => Play(_transform.position, _main.startColor);
+
+        private void OnParticleSystemStopped() => Release();
     }
 }
